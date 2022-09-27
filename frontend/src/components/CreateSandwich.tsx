ff29df1e183e2de9309462e7f97207d566fd259a
Sergej Grilborzer <sergej@grilborzer.de>
2022-09-27 11:37:48 +0200

    docs: Add task summary

--- frontend/src/components/CreateSandwich.tsx
+++ frontend/src/components/CreateSandwich.tsx
@@ -2,10 +2,10 @@ import {useState} from "react";
 import {Sandwich} from "../model/Sandwich";
 
 type CreateSandwichProps = {
-    addSandwich : (description: Sandwich) => void
+    addSandwich: (description: Sandwich) => void
 }
 
-export default function CreateSandwich(props: CreateSandwichProps){
+export default function CreateSandwich(props: CreateSandwichProps) {
 
     const emptySandwichPlaceholder: Sandwich = {
         id: "",
@@ -18,11 +18,28 @@ export default function CreateSandwich(props: CreateSandwichProps){
 
     const [sandwich, setSandwich] = useState(emptySandwichPlaceholder)
 
+    /*
+    * TODO: Aufgabe 3 -> Erstelle eine handleSubmit(event: FormEvent<HTMLFormElement>) Funktion,
+    *  die props.addSandwich aufruft und das neue Sandwich-Objekt als Parameter übergibt
+    **/
+
+    /*
+    * TODO: Aufgabe 2 -> Erstelle eine handleChange(event: ChangeEvent<HTMLInputElement>) Funktion,
+    *  die Änderungen an der Form übernimmt und den Sandwich-State aktualisiert
+    **/
+
+
+    /*
+    * TODO: Aufgabe 1 -> Erstelle eine <form> mit der man alle Daten eines Burgers angeben kann
+    **/
     return (
         <div>
+            {/* TODO: onClick hier entfernen und props.addSandwich in handleSubmit verschieben */}
             <button onClick={() => props.addSandwich(sandwich)}>Bestellung hinzufügen</button>
         </div>
-
     )
 
+    /* TODO: Bonusaufgabe 1 -> Füge dem Projekt Routing hinzu (click auf ein Sandwich, öffnet die Sandwich-Details wie bei Rick&Morty)  */
+    /* TODO: Bonusaufgabe 2 -> Style das Projekt nach deinen Wünschen  */
+    /* TODO: Bonusaufgabe 3 -> Gib dem Sandwich-Objekt mehr Attribute (im Frontend + Backend)  */
 }
