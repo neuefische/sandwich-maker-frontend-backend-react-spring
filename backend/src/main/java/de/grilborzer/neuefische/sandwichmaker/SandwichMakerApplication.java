447b6987c540e948b6f5f021fde2f070420af546
Sergej Grilborzer <sergej@grilborzer.de>
2022-09-28 15:24:17 +0200

    fix: Fix typo in main method

--- backend/src/main/java/de/grilborzer/neuefische/sandwichmaker/SandwichMakerApplication.java
+++ backend/src/main/java/de/grilborzer/neuefische/sandwichmaker/SandwichMakerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SandwitchMakerApplication {
+public class SandwichMakerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SandwitchMakerApplication.class, args);
+        SpringApplication.run(SandwichMakerApplication.class, args);
     }
 
 }
