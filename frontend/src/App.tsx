7b62eefe96eb06ea49dd88c1c3d456a6378c5fe2
Sergej Grilborzer <sergej@grilborzer.de>
2022-09-29 09:46:21 +0200

    FE: Add conditional rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,50 +6,53 @@ import CreateSandwich from "./components/CreateSandwich";
 import {Sandwich} from "./model/Sandwich";
 
 function App() {
-  // Creates a state "sandwiches" and gives us a method to change/set it
-  const [sandwiches, setSandwiches] = useState([]);
+    // Creates a state "sandwiches" and gives us a method to change/set it
+    const [sandwiches, setSandwiches] = useState([]);
 
-  // Load Todos from backend
-  useEffect ( () => {
-    loadSandwiches()
+    // Load Todos from backend
+    useEffect(() => {
+        loadSandwiches()
 
-  } , [] ) // empty dependency array = execute only when website was rendered the first time
+    }, []) // empty dependency array = execute only when website was rendered the first time
 
-  const loadSandwiches = () => {
-    axios.get("/api/sandwich/")
-        .then((response) => response.data)
-        .then((sandwiches) => setSandwiches(sandwiches))
-  }
+    const loadSandwiches = () => {
+        axios.get("/api/sandwich/")
+            .then((response) => response.data)
+            .then((sandwiches) => setSandwiches(sandwiches))
+    }
 
-  const addSandwich = (newSandwich: Sandwich) => {
-    // We use .then here to reload the sandwiches only when the get is done
-    axios.post("/api/sandwich", newSandwich)
-        .then(loadSandwiches) // reload sandwiches from backend
-  }
+    const addSandwich = (newSandwich: Sandwich) => {
+        // We use .then here to reload the sandwiches only when the get is done
+        axios.post("/api/sandwich", newSandwich)
+            .then(loadSandwiches) // reload sandwiches from backend
+    }
 
-  const deleteSandwich = (id: string) => {
+    const deleteSandwich = (id: string) => {
 
-    axios.delete("/api/sandwich/" + id)
-        .then(loadSandwiches) // reload sandwiches from backend
-  }
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Bestellungen</h1>
+        axios.delete("/api/sandwich/" + id)
+            .then(loadSandwiches) // reload sandwiches from backend
+    }
 
-        {/*
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Bestellungen</h1>
+
+                {/*
             TODO: Füge Conditional Rendering hinzu!
             Wenn die sandwiches Liste leer ist, soll ein Text erscheinen
-            der sagt "Bitte Bestellung hinzufügen"
+            der sagt "Bitte eine Bestellung hinzufügen"
         */}
 
-        <SandwichOverview sandwiches={sandwiches} deleteSandwich={deleteSandwich} />
-        <CreateSandwich addSandwich={addSandwich} />
+                {sandwiches.length === 0
+                    && <h3>Bitte bestellen Sie endlich mal, ich habe bald Feierabend und möchte nachhause gehen</h3>}
 
-      </header>
-    </div>
-  );
+                <SandwichOverview sandwiches={sandwiches} deleteSandwich={deleteSandwich}/>
+                <CreateSandwich addSandwich={addSandwich}/>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
