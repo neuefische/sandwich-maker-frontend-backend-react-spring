316fcff34986324a053d1c005c55d1d6685c8ce2
Sergej Grilborzer <sergej@grilborzer.de>
2022-09-29 12:42:06 +0200

    feat(FE): Add form handling and submission

--- frontend/src/components/CreateSandwich.tsx
+++ frontend/src/components/CreateSandwich.tsx
@@ -1,5 +1,6 @@
-import {useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import {Sandwich} from "../model/Sandwich";
+import "./CreateSandwich.css"
 
 type CreateSandwichProps = {
     addSandwich: (description: Sandwich) => void
@@ -22,20 +23,86 @@ export default function CreateSandwich(props: CreateSandwichProps) {
     * TODO: Aufgabe 3 -> Erstelle eine handleSubmit(event: FormEvent<HTMLFormElement>) Funktion,
     *  die props.addSandwich aufruft und das neue Sandwich-Objekt als Parameter übergibt
     **/
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        // Bitte NICHT aktualisiert
+        event.preventDefault()
+
+        props.addSandwich(sandwich)
+    }
 
     /*
-    * TODO: Aufgabe 2 -> Erstelle eine handleChange(event: ChangeEvent<HTMLInputElement>) Funktion,
+    * TODO:  Aufgabe 2 -> Erstelle eine handleChange(event: ChangeEvent<HTMLInputElement>) Funktion,
     *  die Änderungen an der Form übernimmt und den Sandwich-State aktualisiert
     **/
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const inputValue = event.target.value
+        const nameOfTheInputThatChanged = event.target.name
 
+        setSandwich(
+            (prevSandwich) => ({
+                ...prevSandwich,
+                [nameOfTheInputThatChanged]:
+                    event.target.type === "checkbox"
+                        ? event.target.checked
+                        : event.target.value
+            })
+        )
+    }
 
     /*
     * TODO: Aufgabe 1 -> Erstelle eine <form> mit der man alle Daten eines Burgers angeben kann
     **/
     return (
         <div>
-            {/* TODO: onClick hier entfernen und props.addSandwich in handleSubmit verschieben */}
-            <button onClick={() => props.addSandwich(sandwich)}>Bestellung hinzufügen</button>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name der Burgerkreation
+                    <input
+                        name="name"
+                        type="text"
+                        value={sandwich.name}
+                        placeholder="Big Boss"
+                        onChange={handleChange}
+                    />
+                </label>
+                <br/>
+
+                <label>
+                    Wunsch-Patty
+                    <input
+                        name="patty"
+                        type="text"
+                        value={sandwich.patty}
+                        placeholder="Vegane Rinderbulette"
+                        onChange={handleChange}
+                    />
+                    <br/>
+                </label>
+
+                <label>
+                    Anzahl an Patties
+                    <input
+                        name="numberOfPatties"
+                        type="number"
+                        value={sandwich.numberOfPatties}
+                        onChange={handleChange}
+                    />
+                    <br/>
+                </label>
+
+                <label>
+                    Mit oder ohne gegrillt?
+                    <input
+                        name="grilled"
+                        type="checkbox"
+                        checked={sandwich.grilled}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <br/>
+                <button type={"submit"}>Bestellung hinzufügen</button>
+            </form>
         </div>
     )
 
